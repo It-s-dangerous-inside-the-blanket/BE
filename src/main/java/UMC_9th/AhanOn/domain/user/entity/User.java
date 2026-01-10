c89e46b20e7fc7009ad7400e67a16313b031c8b8
Kwon-DoHee <152317074+seamooll@users.noreply.github.com>
2026-01-11 00:52:55 +0900

    FEAT: 회원가입/로그인 API 및 닉네임 중복 체크 API 구현

--- src/main/java/UMC_9th/AhanOn/domain/user/entity/User.java
+++ src/main/java/UMC_9th/AhanOn/domain/user/entity/User.java
@@ -5,6 +5,7 @@ import UMC_9th.AhanOn.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
@@ -20,6 +21,7 @@ import java.util.List;
                 @Index(name = "ux_users_nickname", columnList = "nickname", unique = true)
         }
 )
+@Getter
 public class User extends BaseEntity {
 
     @Id
@@ -30,7 +32,7 @@ public class User extends BaseEntity {
     @Column(name = "nickname", nullable = false, length = 50)
     private String nickname;
 
-    @Column(name = "password", nullable = false, length = 50)
+    @Column(name = "password", nullable = false, length = 60)
     private String password;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
