dfb62614f1a2182314443d4a75110dbc74f75e97
leesuyong849 <toosee1005@cau.ac.kr>
2026-01-10 23:34:10 +0900

    FEAT: hashtag create 관련된 코드 작성

---
+++ src/main/java/UMC_9th/AhanOn/domain/book/repository/BookRepository.java
@@ -0,0 +1,11 @@
+package UMC_9th.AhanOn.domain.book.repository;
+
+import UMC_9th.AhanOn.domain.book.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+    Optional<Book> findById(Long id);
+}
--- src/main/java/UMC_9th/AhanOn/domain/book/service/HashtagService.java
+++ src/main/java/UMC_9th/AhanOn/domain/book/service/HashtagService.java
@@ -1,8 +1,11 @@
 package UMC_9th.AhanOn.domain.book.service;
 
+import UMC_9th.AhanOn.domain.book.code.HashErrorCode;
 import UMC_9th.AhanOn.domain.book.entity.Book;
 import UMC_9th.AhanOn.domain.book.entity.Hashtag;
 import UMC_9th.AhanOn.domain.book.entity.dto.HashtagDto;
+import UMC_9th.AhanOn.domain.book.exception.HashtagException;
+import UMC_9th.AhanOn.domain.book.repository.BookRepository;
 import UMC_9th.AhanOn.domain.book.repository.HashtagRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,23 +20,27 @@ import java.util.Optional;
 public class HashtagService {
 
     private final HashtagRepository hashtagRepository;
-//    private final BookRepository bookRepository;
-//
-//    @Transactional
-//    public Hashtag createHashtag(HashtagDto.CreateRequest request) {
-//
-//        bookRepository.findById(request.getBookId()).orElseThrow(
-//                () -> new
-//        );
-//
-//        boolean existsByBookIdAndHashtag = hashtagRepository.existsByBookIdAndHashtag(book.getId(), hashtag);
-//        if (existsByBookIdAndHashtag) return hashtagRepository.findByBookIdAndHashtag(book.getId(), hashtag).get();
-//
-//        return Hashtag.builder()
-//                .hashtag(hashtag)
-//                .book(book)
-//                .build();
-//    }
+    private final BookRepository bookRepository;
+
+    @Transactional
+    public Hashtag createHashtag(HashtagDto.CreateRequest request) {
+
+        Book book = bookRepository.findById(request.getBookId()).orElseThrow(
+                () -> new HashtagException(HashErrorCode.NOT_FOUND_HASHTAG)
+        );
+
+        String hashtag = request.getHashtag();
+
+        nomerization(hashtag);
+
+        boolean existsByBookIdAndHashtag = hashtagRepository.existsByBookIdAndHashtag(book.getId(), hashtag);
+        if (existsByBookIdAndHashtag) return hashtagRepository.findByBookIdAndHashtag(book.getId(), hashtag).get();
+
+        return Hashtag.builder()
+                .hashtag(hashtag)
+                .book(book)
+                .build();
+    }
 
     @Transactional
     public void deleteHashtag(Book book, String hashtag) {
@@ -44,5 +51,11 @@ public class HashtagService {
 
     public List<Hashtag> findAllByBook(Book book) {
         return hashtagRepository.findAllByBookId(book.getId());
+    }
+
+    private void nomerization(String str) {
+        if (str == null || str.isBlank()) {
+            throw new HashtagException(HashErrorCode.BAD_REQUEST_HASHTAG_EMPTY);
+        }
     }
 }
