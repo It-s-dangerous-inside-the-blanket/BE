a09a67e60bd5ff114d4e27632f82c0b8e35a6eb1
leesuyong849 <toosee1005@cau.ac.kr>
2026-01-11 01:50:58 +0900

    FEAT: @Operation 수정

--- src/main/java/UMC_9th/AhanOn/domain/book/controller/BookController.java
+++ src/main/java/UMC_9th/AhanOn/domain/book/controller/BookController.java
@@ -5,6 +5,7 @@ import UMC_9th.AhanOn.domain.book.entity.dto.BookDto;
 import UMC_9th.AhanOn.domain.book.service.BookService;
 import UMC_9th.AhanOn.global.apiPayload.ApiResponse;
 import UMC_9th.AhanOn.global.apiPayload.code.GeneralSuccessCode;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -18,18 +19,30 @@ public class BookController {
 
     private final BookService bookService;
 
+    @Operation(
+            summary = "책 생성",
+            description = "사용자(userId)의 책을 생성합니다."
+    )
     @PostMapping("/create")
     public ApiResponse<BookDto.Response> createBook(@RequestBody BookDto.CreateRequest request, Long userId) {
         BookDto.Response book = bookService.createBook(request, userId);
         return ApiResponse.onSuccess(GeneralSuccessCode.OK, book);
     }
 
+    @Operation(
+            summary = "책 완료 처리",
+            description = "bookId에 해당하는 책을 완료 상태로 변경합니다."
+    )
     @PatchMapping("/completed/{bookId}")
     public ApiResponse<BookDto.Response> updateCompleted(@PathVariable Long bookId) {
         BookDto.Response response = bookService.updateCompleteBook(bookId, true);
         return ApiResponse.onSuccess(GeneralSuccessCode.OK, response);
     }
 
+    @Operation(
+            summary = "내 책 목록 조회",
+            description = "userId가 소유한 책 목록을 조회합니다."
+    )
     @GetMapping("/list/{userId}")
     public ApiResponse<List<BookDto.Response>> searchMyBook(@PathVariable Long userId) {
         List<BookDto.Response> response = bookService.searchBookList(userId);
--- src/main/java/UMC_9th/AhanOn/domain/book/controller/HashtagController.java
+++ src/main/java/UMC_9th/AhanOn/domain/book/controller/HashtagController.java
@@ -5,6 +5,7 @@ import UMC_9th.AhanOn.domain.book.entity.dto.HashtagDto;
 import UMC_9th.AhanOn.domain.book.service.HashtagService;
 import UMC_9th.AhanOn.global.apiPayload.ApiResponse;
 import UMC_9th.AhanOn.global.apiPayload.code.GeneralSuccessCode;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,12 +16,20 @@ public class HashtagController {
 
     private final HashtagService hashtagService;
 
+    @Operation(
+            summary = "해시태그 생성",
+            description = "책에 새로운 해시태그를 생성합니다."
+    )
     @PostMapping("/create")
     public ApiResponse<Hashtag> createHashtag(@RequestBody HashtagDto.Request request) {
         Hashtag hashtag = hashtagService.createHashtag(request);
         return ApiResponse.onSuccess(GeneralSuccessCode.OK, hashtag);
     }
 
+    @Operation(
+            summary = "해시태그 삭제",
+            description = "요청 정보에 해당하는 해시태그를 삭제합니다."
+    )
     @DeleteMapping("delete")
     public ApiResponse<Void> deleteHashtag(@RequestBody HashtagDto.Request request) {
         hashtagService.deleteHashtag(request);
