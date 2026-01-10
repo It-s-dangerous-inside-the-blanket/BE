18d049baca7a5cfc3cca883ed3994edb954348f2
jijysun <jijysun@gmail.com>
2026-01-10 17:18:39 +0900

    project init

---
+++ src/test/java/UMC_9th/AhanOn/AhanOnApplicationTests.java
@@ -0,0 +1,13 @@
+package UMC_9th.AhanOn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhanOnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
